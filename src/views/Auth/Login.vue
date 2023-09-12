1a0867a0f0426c00ba1e84d52a2e35097eda52d5
Jeremy Holstein <jjjrmy@gmail.com>
2023-09-11 20:37:38 -0400

    error

--- src/views/Auth/Login.vue
+++ src/views/Auth/Login.vue
@@ -1,14 +1,27 @@
 <template>
   <Head title="Login Here" />
-  <main class="h-full min-h-screen bg-gray-900 flex flex-col justify-center px-6 py-12 lg:px-8">
-    <section class="sm:mx-auto sm:w-full sm:max-w-sm">
-      <Logo class="mx-auto" />
-      <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">Sign in to your account</h2>
-    </section>
+  <main class="h-full min-h-screen bg-gray-900 flex flex-col space-y-10 justify-center px-6 py-12 lg:px-8">
+    <Logo class="mx-auto" />
+    <h2 class="text-center text-2xl font-bold leading-9 tracking-tight text-white">Sign in to your account</h2>
 
-    <!--<pre class="text-red-500">{{ usePage().props.flash.error }}</pre>-->
+    <article v-if="!!usePage().props?.flash?.error" class="sm:mx-auto sm:w-full sm:max-w-sm | flex rounded-md bg-red-600 border border-red-500 p-4 space-x-3">
+      <div class="flex-shrink-0">
+        <Icon icon="mdi:close-octagon-outline" class="h-5 w-5 text-red-900" />
+      </div>
+      <div>
+        <p class="text-sm font-medium text-red-900">{{ usePage().props.flash.error }}</p>
+      </div>
+      <div class="!ml-auto">
+        <div class="-mx-1.5 -my-1.5">
+          <button type="button" class="inline-flex rounded-md bg-red-600 p-1.5 text-red-800 hover:bg-red-700/50 focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-offset-2 focus:ring-offset-red-500">
+            <span class="sr-only">Dismiss</span>
+            <Icon icon="mdi:close" class="h-5 w-5" />
+          </button>
+        </div>
+      </div>
+    </article>
 
-    <FormKit type="form" @submit="login" formClass="mt-10 sm:mx-auto sm:w-full sm:max-w-sm space-y-6" :actions="false"
+    <FormKit type="form" @submit="login" formClass="sm:mx-auto sm:w-full sm:max-w-sm space-y-6" :actions="false"
       :value="{ email: 'johndoe@example.com', password: 'secret' }"
     >
       <FormKit type="email" name="email" label="Email address" innerClass="$remove:ring-gray-300 ring-white/10" inputClass="$remove:text-gray-900 text-gray-50" labelClass="text-white" />
